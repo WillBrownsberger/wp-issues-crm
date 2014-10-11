4b5a6af98291f1ca83777a5197a71edc59cfcd4c
WillBrownsberger <WillBrownsberger@gmail.com>
2014-10-11 16:51:16 -0400

    through multi addresses

---
+++ class-wp-issues-crm-import-routines.php
@@ -0,0 +1,94 @@
+<?php
+  /* 
+   *
+   *
+   *
+   * import 
+   *
+   */
+   function import(){ // fields must include so formatted first_name, last_name, email
+	   // NEEDS UPDATEING TO REFLECT ONLINE EXCLUSION
+	   $i=0;
+	   $j=0;
+	   $seconds = 5000;
+		set_time_limit ( $seconds );
+	   
+	   global $wpdb;
+	   $contacts = $wpdb->get_results( 'select * from wp_swc_contacts' );
+	   foreach ($contacts as $contact ) {
+			if ( $i/1000 == floor($i/1000 ) ) {
+				echo '<h3>' . $i . ' records processed</h3>';			
+			}	   
+		   $i++;
+		   // if ($i>10) break;
+		   			
+			$post_information = array(
+				'post_title' => wp_strip_all_tags ( $contact->last_name . ', ' . $contact->first_name ),
+				'post_type' => 'wic_constituent',
+				'post_status' => 'private',
+				'post_author' => 15,
+				'ping_status' => 'closed',
+			);
+		
+			$post_id = wp_insert_post($post_information);
+			
+			foreach ($this->constituent_fields as $field) {			
+				if ( isset ( $contact->$field['slug'] ) ) {
+					if ( $contact->$field['slug'] > '' ) {
+						$stored_record = add_post_meta ($post_id, $this->wic_metakey . $field['slug'], $contact->$field['slug'] );
+						if ( $stored_record ) {
+							$j++;						
+						}
+					}				
+				} 
+			}
+		}
+		echo '<h1>' . $i . ' constituent records in total processed</h1>';
+		echo '<h1>' . $j . ' meta records in total stored</h1>';
+	}
+class WP_Issues_CRM_Import_Routines {
+
+	public function run_phone_cleanup() {
+			   $i=0;
+	   $j=0;
+	   $seconds = 5000;
+		set_time_limit ( $seconds );
+	   
+	   global $wpdb;
+	   $contacts = $wpdb->get_results( 'SELECT p.id as ID, max(m1.meta_value) as phone, max(m2.meta_value) as mobile from wp_posts p 
+	   											left join wp_postmeta m1 on m1.post_id = p.ID and m1.meta_key = "wic_data_phone"
+	   											left join wp_postmeta m2 on m2.post_ID = p.ID and m2.meta_key = "wic_data_mobile_phone"
+	   											group by p.ID having max(m1.meta_value) is not null or max(m2.meta_value) is not null' );
+	   foreach ($contacts as $contact ) {
+			if ( $i/1000 == floor($i/1000 ) ) {
+				echo '<h3>' . $i . ' records processed</h3>';			
+			}	   
+		   $i++;
+		   // if ($i>10) break;
+		   			
+			
+			$phone_array = array ();
+			$index = 0;
+				if ( preg_replace("/[^0-9]/", '', $contact->phone ) > '' )
+					{ 
+						$phone_array[$index] = array( '0' , preg_replace("/[^0-9]/", '', $contact->phone ), '' );
+						$index++; 
+					}
+				if ( preg_replace("/[^0-9]/", '', $contact->mobile ) > '0' )
+					{ 
+						$phone_array[$index] = array( '0' , preg_replace("/[^0-9]/", '', $contact->mobile ), '' );
+						$index++; 
+					}			   
+				
+			/* var_dump($phone_array);
+			echo '<br />'; */
+			if ( $index > 0 ) {
+				add_post_meta ($contact->ID, 'wic_data_phone_numbers', $phone_array );
+			} 
+		}
+
+	}	
+	
+}
+
+$wic_imports = new WP_Issues_CRM_Import_Routines;
