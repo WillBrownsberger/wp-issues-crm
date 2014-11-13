6ec05cb6d5d7ddf886c3031b2ecf690e26d30dc5
WillBrownsberger <WillBrownsberger@gmail.com>
2014-11-12 22:08:32 -0500

    in debugging mode for CRUD

---
+++ php/form/class-wic-form-multivalue-save.php
@@ -0,0 +1,22 @@
+<?php
+/*
+*
+*  class-wic-form-multivalue-update.php
+*
+*	this form is for an instance of a multivalue field -- a row (or rows, if multiple groups) of controls, not a full form
+*	  
+*	entity in this context is the entity that the multivalue field may contain several instances of 
+*   -- this form generator doesn't need to know the instance value; 
+* 	 -- the control objects within each row know which row they are implementing
+*
+*/
+
+class WIC_Form_Multivalue_Save extends WIC_Form_Multivalue_Update  {
+
+	protected function get_the_formatted_control ( $control ) {
+		$args = array(
+			'onclick_delete' => 'hideSelf(\'' . esc_attr( $this->entity . '[' . $this->entity_instance . ']'  ) . '\')',		
+		);
+		return ( $control->save_control( $args ) ); 
+	}
+}
