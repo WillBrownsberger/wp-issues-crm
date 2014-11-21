9e97ef3d75417b32f595b8fc9720ac2ace10b209
WillBrownsberger <WillBrownsberger@gmail.com>
2014-11-21 06:36:33 -0500

    zip lookup

---
+++ php/entity/class-wic-entity-address-usps-account.php
@@ -0,0 +1,16 @@
+<?php
+/*
+*
+* adapted from http://joe-riggs.com/blog/2009/10/address-standardization-verification-with-usps-web-tools-and-php/
+*
+*/ 
+ 
+class WIC_Entity_Address_USPS_Account {
+ 
+	public static function get_usps_account() {
+		return '565MASSA1058'; 
+	} 
+ 
+}
+ 
+?>
