84b5774a7d345ccef53800f739d5b0e425d5f0dc
WillBrownsberger <WillBrownsberger@gmail.com>
2014-12-07 12:59:22 -0500

    punch list I

---
+++ php/form/class-wic-form-constituent-search-again.php
@@ -0,0 +1,44 @@
+<?php
+/*
+*
+*  class-wic-search-form-again.php
+*
+*/
+
+class WIC_Form_Constituent_Search_Again extends WIC_Form_Constituent_Search  {
+	
+	// differ from search form only in button definition
+	protected function get_the_buttons () {
+
+		$buttons = '';
+		
+		$button_args_main = array(
+			'entity_requested'			=> 'constituent',
+			'action_requested'			=> 'form_search',
+			'button_label'					=> __('Search Again', 'wp-issues-crm')
+		);	
+		
+		$buttons .= $this->create_wic_form_button ( $button_args_main );
+
+		$button_args_main = array(
+			'entity_requested'			=> 'constituent',
+			'action_requested'			=> 'save_from_search',
+			'button_class'					=> 'wic-form-button second-position',
+			'button_label'					=> __('Save New', 'wp-issues-crm')
+		);	
+		
+		$buttons .=  $this->create_wic_form_button ( $button_args_main );
+
+		$button_args_main = array(
+			'entity_requested'			=> 'constituent',
+			'action_requested'			=> 'new_form',
+			'button_class'					=> 'wic-form-button second-position',
+			'button_label'					=> __('Start Over', 'wp-issues-crm')
+		);	
+		
+		$buttons .=  $this->create_wic_form_button ( $button_args_main );		
+		
+		return $buttons;
+	}
+	
+}
---
+++ php/form/class-wic-form-issue-search-again.php
@@ -0,0 +1,45 @@
+<?php
+/*
+*
+*  class-wic-form-issue-search-again.php
+*
+*/
+
+class WIC_Form_issue_Search_Again extends WIC_Form_issue_Search  {
+	
+
+	// choose form buttons
+	protected function get_the_buttons () {
+
+		$buttons = '';
+		
+		$button_args_main = array(
+			'entity_requested'			=> 'issue',
+			'action_requested'			=> 'form_search',
+			'button_label'					=> __('Search Again', 'wp-issues-crm')
+		);	
+		
+		$buttons .= $this->create_wic_form_button ( $button_args_main );
+
+		$button_args_main = array(
+			'entity_requested'			=> 'issue',
+			'action_requested'			=> 'save_from_search',
+			'button_class'					=> 'wic-form-button second-position',
+			'button_label'					=> __('Save New', 'wp-issues-crm')
+		);	
+		
+		$buttons .=  $this->create_wic_form_button ( $button_args_main );
+
+		$button_args_main = array(
+			'entity_requested'			=> 'issue',
+			'action_requested'			=> 'new_form',
+			'button_class'					=> 'wic-form-button second-position',
+			'button_label'					=> __('Start Over', 'wp-issues-crm')
+		);	
+		
+		$buttons .=  $this->create_wic_form_button ( $button_args_main );		
+		
+		return $buttons;
+	}
+
+}
